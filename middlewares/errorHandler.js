b8bc326ba49743b0cb87d443d3477b434ebb7be7
Ejay02 <e.jae02@gmail.com>
2024-07-25 14:20:18 +0100

    modifiction and updates: added cors and modified models

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -11,7 +11,7 @@ const errorHandler = (err, req, res, next) => {
   res.json({
     message: err?.message,
     // stack: err?.stack
-    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack
   });
 };
 
