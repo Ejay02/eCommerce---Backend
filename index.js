cb3697318f9c8c8fa0e2732bf42a06c74a127ad3
Ejay02 <e.jae02@gmail.com>
2024-10-04 09:07:55 +0100

    updated cors origin options

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const corsOptions = {
     'http://localhost:5174',
     'http://localhost:5175',
     'http://localhost:5176',
+    'http://127.0.0.1:5173',
     'https://buyzone-demo.netlify.app',
     'https://buyzone-admin-dashboard.netlify.app'
   ],
